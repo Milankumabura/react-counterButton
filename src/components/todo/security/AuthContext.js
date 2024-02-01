eb1c1f6104d889be0937462ad9c9fedf12c7428a
milan <milanbudha007.mgr@gmail.com>
2024-02-01 11:48:23 +0545

    authcontext

--- src/components/todo/security/AuthContext.js
+++ src/components/todo/security/AuthContext.js
@@ -1,16 +1,33 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 //1.create a context
 export const AuthContext = createContext()
+export const useAuth = () => useContext(AuthContext)
 
 //2.shared the created context with other components
 export default function AuthProvider({ children }) {
 
-    //put some state in the context
-    const[number, setNumber] = useState(10)
+    //3.put some state in the context
+
+    const[isAuthenticated, setAuthenticated] = useState(false)
+
+    function login(username, password) {
+        if(username === 'milan' && password === 'dummy') {
+            setAuthenticated(true)
+            return true
+        }
+        else {
+            setAuthenticated(false)
+            return false
+        }
+    }
+
+    function logout() {
+        setAuthenticated(false)
+    }
 
     return(
-        <AuthContext.Provider value={{ number }}>
+        <AuthContext.Provider value={ { isAuthenticated, login, logout} }>
             {children}
         </AuthContext.Provider>
     )
