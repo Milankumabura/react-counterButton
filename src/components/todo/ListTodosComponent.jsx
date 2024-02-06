376f95e8768cddd5a80ca89401fb5222020ad1d5
milan <milanbudha007.mgr@gmail.com>
2024-02-06 16:17:15 +0545

    updating todo

--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { retrieveAllTodosForUsernameApi, deleteTodoApi }from "./api/TodoApiService"
 import { useAuth } from "./security/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function ListTodosComponent() {
 
@@ -9,6 +10,8 @@ function ListTodosComponent() {
     const authContext = useAuth()
 
     const username = authContext.username
+
+    const navigate = useNavigate()
 
     const targetDate = new Date(today.getFullYear()+12, today.getMonth() + today.getDay())
 
@@ -40,6 +43,12 @@ function ListTodosComponent() {
         )
         .catch(error => console.log("error"))
     }
+
+    function updateTodo(id) {
+        console.log('clicked '+ id)
+        navigate(`/todo/${id}`)
+        
+    }
             
 
     return(
@@ -55,6 +64,7 @@ function ListTodosComponent() {
                             <th>Is Done?</th>
                             <th>Target Date</th>
                             <th>Delete</th>
+                            <th>Update</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -68,6 +78,8 @@ function ListTodosComponent() {
                                     <td>{todo.targetDate.toDateString()}</td>
                                     <td><button className="btn btn-warning" 
                                         onClick={() => deleteTodo(todo.id)}>Delete</button></td>
+                                    <td><button className="btn btn-success" 
+                                        onClick={() => updateTodo(todo.id)}>Update</button></td>
                                 </tr>
                             )
                         )
